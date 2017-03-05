3cdbf500367659cabab216b658403e84fea4a301
Unknown <jonstodle@outlook.com>
2017-03-05 02:04:48 +0100

    Add realm objects

---
+++ RealmTodo/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Realms;
+
+namespace RealmTodo
+{
+	public class Person : RealmObject
+	{
+		[PrimaryKey]
+		public string Id
+		{
+			get;
+			set;
+		} = Guid.NewGuid().ToString();
+
+		[Indexed]
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		[Backlink(nameof(Todo.Assignee))]
+		public IQueryable<Todo> Todos
+		{
+			get;
+		}
+	}
+}
---
+++ RealmTodo/Todo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Realms;
+
+namespace RealmTodo
+{
+	public class Todo : RealmObject
+	{
+		[PrimaryKey]
+		public string Id
+		{
+			get;
+			set;
+		} = Guid.NewGuid().ToString();
+
+		[Indexed]
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public bool IsDone
+		{
+			get;
+			set;
+		}
+
+		public Person Assignee
+		{
+			get;
+			set;
+		}
+	}
+}
